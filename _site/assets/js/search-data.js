4ca72f118773d97d5c85af880560faa53352da7d
Cesar Castillo <cecastillo@uchile.cl>
2025-08-09 20:08:21 -0400

    Actualización de post de prueba y ajustes

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Post De Prueba",
-        "excerpt":"¡Hola a todos! Este es el contenido de mi primer post de prueba. He creado este post para verificar que mi sitio de Hugo está funcionando correctamente con el tema Anatole. Encabezados Aquí hay un ejemplo de diferentes niveles de encabezados para ver si se muestran bien. Subtítulo Nivel 4...","categories": ["jekyll"],
-        "tags": ["primer","tutorial"],
-        "url": "http://localhost:4000/la-sala-del-lado/jekyll/2023/10/27/prueba.html"
+        "title": "Post de Prueba",
+        "excerpt":"¡Hola a todos! Este es el contenido de mi primer post de prueba. He creado este post para verificar que mi sitio de Hugo está funcionando correctamente con el tema Anatole. Encabezados Aquí hay un ejemplo de diferentes niveles de encabezados para ver si se muestran bien. Subtítulo Nivel 4...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/la-sala-del-lado/2025/08/07/prueba.html"
       }]
